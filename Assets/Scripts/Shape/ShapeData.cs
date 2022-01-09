ed099cf604353d43920854b0f8eb96f01b3f4f00
Albert Medov <polpybert@gmail.com>
2022-01-09 19:15:32 +0200

    Implemented editor for shapes.

--- Assets/Scripts/Shape/ShapeData.cs
+++ Assets/Scripts/Shape/ShapeData.cs
@@ -2,17 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShapeData : MonoBehaviour
+[CreateAssetMenu]
+[System.Serializable]
+
+public class ShapeData : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
+    [System.Serializable]
+    public class Row
     {
+        public bool[] column;
+        private int _size = 0;
         
+        public Row(){}
+
+        public Row(int size)
+        {
+            CreateRow(size);
+        }
+
+        public void CreateRow(int size)
+        {
+            _size = size;
+            column = new bool[_size];
+            ClearRow();
+        }
+
+        public void ClearRow()
+        {
+            for (int i = 0; i < _size; i++)
+            {
+                column[i] = false;
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public int columns = 0;
+    public int rows = 0;
+    public Row[] board;
+
+    public void Clear()
     {
-        
+        for (int i = 0; i < rows; i++)
+        {
+            board[i].ClearRow();
+        }
+    }
+
+    public void CreateNewBoard()
+    {
+        board = new Row[rows];
+        for (int i = 0; i < rows; i++)
+        {
+            board[i] = new Row(columns);
+        }
     }
 }
