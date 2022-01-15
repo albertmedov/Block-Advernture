0f44c4e93cdaa57e4c305e55bc778e81bd9f91fa
Albert Medov <albert.medov@murka.com>
2022-01-15 18:23:25 +0200

    Tutorial 5 - done.
    Now shapes are spawns in the gameScene.

--- Assets/Scripts/Shape/Shape.cs
+++ Assets/Scripts/Shape/Shape.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,16 +6,114 @@ using UnityEngine;
 public class Shape : MonoBehaviour
 {
     public GameObject SquareShapeImage;
-    [HideInInspector] 
     public ShapeData CurrentShapeData;
     
     private List<GameObject> _currentShape = new List<GameObject>();
-    void Start()
+
+    private void Start()
     {
-        
+        RequestNewShape(CurrentShapeData);
+    }
+    private void RequestNewShape(ShapeData shapeData)
+    {
+        CreateShape(shapeData);
+    }
+    
+    public void CreateShape(ShapeData shapeData)
+    {
+        CurrentShapeData = shapeData;
+        var totalSquareNumber = GetNumberOfSquares(shapeData);
+        while (_currentShape.Count <= totalSquareNumber)
+        {
+            _currentShape.Add(Instantiate(SquareShapeImage, transform) as GameObject);
+        }
+
+        foreach (var square in _currentShape)
+        {
+            square.gameObject.transform.position = Vector3.zero;
+            square.gameObject.SetActive(false);
+        }
+
+        var squareRect = SquareShapeImage.GetComponent<RectTransform>();
+        var moveDistance = new Vector2(squareRect.rect.width * squareRect.localScale.x, squareRect.rect.height * squareRect.localScale.y);
+
+        int currentIndexInList = 0;
+
+        for (var row = 0; row < shapeData.rows; row++)
+        {
+            for (var column = 0; column < shapeData.columns; column++)
+            {
+                if (shapeData.board[row].column[column])
+                {
+                    _currentShape[currentIndexInList].SetActive(true);
+                    _currentShape[currentIndexInList].GetComponent<RectTransform>().localPosition =
+                        new Vector2(GetXPositionForShapeSquare(shapeData, column, moveDistance),
+                            GetYPositionForShapeSquare(shapeData, row, moveDistance));
+
+                    currentIndexInList++;
+                }
+            }
+        }
     }
 
-    public float GetXpositionForShapeSquare(ShapeData shapeData, int column, Vector2 moveDsitance)
+    private float GetYPositionForShapeSquare(ShapeData shapeData, int row, Vector2 moveDistance)
+    {
+        float shiftOnY = 0f;
+
+        if (shapeData.rows > 1)
+        {
+            if (shapeData.rows % 2 != 0)
+            {
+                var middleSquareIndex = (shapeData.rows - 1) / 2;
+                var multiplier = (shapeData.rows - 1) / 2;
+
+                if (row < middleSquareIndex)
+                {
+                    shiftOnY = moveDistance.y * 1;
+                    shiftOnY *= multiplier;
+                }
+                else if (row > middleSquareIndex)
+                {
+                    shiftOnY = moveDistance.y * -1;
+                    shiftOnY *= multiplier;
+                }
+            }
+            else
+            {
+                var middleSquareIndex2 = (shapeData.rows == 2) ? 1 : (shapeData.rows / 2);
+                var middleSquareIndex1 = (shapeData.rows == 2) ? 0 : shapeData.rows - 2;
+                var multiplier = shapeData.rows / 2;
+
+                if (row == middleSquareIndex1 || row == middleSquareIndex2)
+                {
+                    if (row == middleSquareIndex2)
+                    {
+                        shiftOnY = (moveDistance.y / 2) * -1;
+                    }
+
+                    if (row == middleSquareIndex1)
+                    {
+                        shiftOnY = (moveDistance.y / 2);
+                    }
+                }
+                if (row < middleSquareIndex1 && row < middleSquareIndex2)
+                {
+                    shiftOnY = moveDistance.y * 1;
+                    shiftOnY *= multiplier;
+                }
+                else if (row > middleSquareIndex1 && row > middleSquareIndex2)
+                {
+                    shiftOnY = moveDistance.y * -1;
+                    shiftOnY *= multiplier;
+                }
+            }
+        }
+        return shiftOnY;
+    }
+
+
+
+    private float GetXPositionForShapeSquare(ShapeData shapeData, int column, Vector2 moveDistance)
     {
         float shiftonX = 0f;
         if (shapeData.columns > 1)
@@ -23,39 +122,40 @@ public class Shape : MonoBehaviour
             {
                 var middleSquareIndex = (shapeData.columns - 1) / 2;
                 var multiplier = (shapeData.columns - 1) / 2;
+                
                 if (column < middleSquareIndex)
                 {
-                    shiftonX = moveDsitance.x * -1;
+                    shiftonX = moveDistance.x * -1;
                     shiftonX *= multiplier;
                 }
                 else if (column > middleSquareIndex)
                 {
-                    shiftonX = moveDsitance.x * 1;
+                    shiftonX = moveDistance.x * 1;
                     shiftonX *= multiplier;
                 }
-                else
+            }
+            else
+            {
+                var middleSquareIndex2 = (shapeData.columns == 2) ? 1 : (shapeData.columns / 2);
+                var middleSquareIndex1 = (shapeData.columns == 2) ? 0 : shapeData.columns - 2;
+                var multiplier = shapeData.columns / 2;
+                if (column == middleSquareIndex1 || column == middleSquareIndex2)
                 {
-                    var middleSquareIndex2 = (shapeData.columns == 2) ? 1 : (shapeData.columns / 2);
-                    var middleSquareIndex1 = (shapeData.columns == 2) ? 0 : shapeData.columns - 2;
-                    var multiplier = shapeData.columns / 2;
-                    if (column == middleSquareIndex1 || column == middleSquareIndex2)
-                    {
-                        if (column == middleSquareIndex2)
-                            shiftonX = moveDsitance.x / 2;
-                        if (column == middleSquareIndex1)
-                            shiftonX = (moveDsitance.x / 2) * -1;
-                    }
+                    if (column == middleSquareIndex2)
+                        shiftonX = moveDistance.x / 2;
+                    if (column == middleSquareIndex1)
+                        shiftonX = (moveDistance.x / 2) * -1;
+                }
 
-                    if (column < middleSquareIndex1 && column < middleSquareIndex2)
-                    {
-                        shiftonX = moveDsitance.x * -1;
-                        shiftonX *= multiplier;
-                    }
-                    else if (column > middleSquareIndex1 && column > middleSquareIndex2)
-                    {
-                        shiftonX = moveDsitance.x * 1;
-                        shiftonX *= multiplier;
-                    }
+                if (column < middleSquareIndex1 && column < middleSquareIndex2)
+                {
+                    shiftonX = moveDistance.x * -1;
+                    shiftonX *= multiplier;
+                }
+                else if (column > middleSquareIndex1 && column > middleSquareIndex2)
+                {
+                    shiftonX = moveDistance.x * 1;
+                    shiftonX *= multiplier;
                 }
             }
         }
@@ -75,8 +175,6 @@ public class Shape : MonoBehaviour
                     number++;
             }
         }
-
         return number;
     }
-    
 }
