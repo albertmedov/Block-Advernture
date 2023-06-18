25fdb57a8d981d69a6cf1e7b7b5658dff3469343
Albert Medov <albert.medov@murka.com>
2023-06-18 14:20:10 +0200

    Tutorial 18 - done.

--- Assets/Scripts/GameCore/Scores.cs
+++ Assets/Scripts/GameCore/Scores.cs
@@ -4,35 +4,77 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[Serializable]
+public class BestScoreData
+{
+    public int score = 0;
+}
+
 public class Scores : MonoBehaviour
 {
     public Text scoreText;
 
-    private int currentScores_; 
+    private bool newBestScore_;
+    
+    private BestScoreData bestScores_ = new BestScoreData();
+    
+    private int currentScores_;
+
+    private string bestScoreKey_ = "bsdat";
+
+    private void Awake()
+    {
+        if (BinaryDataStream.Exist(bestScoreKey_))
+        {
+            StartCoroutine(ReadDataFile());
+        }
+    }
+
+    private IEnumerator ReadDataFile()
+    {
+        bestScores_ = BinaryDataStream.Read<BestScoreData>(bestScoreKey_);
+        yield return new WaitForEndOfFrame();
+        Debug.Log("Read Best Scores = " + bestScores_.score);
+    }
+
     void Start()
     {
-
         currentScores_ = 0;
-        UpdateScoretext();
+        newBestScore_ = false;
+        UpdateScoreText();
     }
 
     private void OnEnable()
     {
         GameEvents.AddScores += AddScores;
+        GameEvents.GameOver += SaveBestScores;
+
     }
 
     private void OnDisable()
     {
         GameEvents.AddScores -= AddScores;
+        GameEvents.GameOver -= SaveBestScores;
+
+    }
+
+    public void SaveBestScores(bool newBestScores)
+    {
+        BinaryDataStream.Save<BestScoreData>(bestScores_, bestScoreKey_);
     }
 
     private void AddScores(int scores)
     {
         currentScores_ += scores;
-        UpdateScoretext();
+        if (currentScores_ > bestScores_.score)
+        {
+            newBestScore_ = true;
+            bestScores_.score = currentScores_;
+        }
+        UpdateScoreText();
     }
 
-    private void UpdateScoretext()
+    private void UpdateScoreText()
     {
         scoreText.text = currentScores_.ToString();
     }
